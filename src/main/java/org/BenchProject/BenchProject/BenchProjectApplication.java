b363905962d2080e1419d26cc5e2b276283ef2d4
HPeterf <horvathpeterf@hotmail.com>
2018-02-06 17:34:09 +0100

    elso kommit

---
+++ src/main/java/org/BenchProject/BenchProject/BenchProjectApplication.java
@@ -0,0 +1,12 @@
+package org.BenchProject.BenchProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/org/BenchProject/BenchProject/Endpoint.java
@@ -0,0 +1,14 @@
+package org.BenchProject.BenchProject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Endpoint {
+
+	@RequestMapping("/name")
+	public String index() {
+		return "Horváth Péter";
+	}
+
+}
