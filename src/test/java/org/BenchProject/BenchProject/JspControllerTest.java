3919025552e196b26a2dec5a6372476ae7564be1
HPeterf <horvathpeterf@hotmail.com>
2018-02-07 16:52:14 +0100

    unit tesztek

---
+++ src/test/java/org/BenchProject/BenchProject/JspControllerTest.java
@@ -0,0 +1,29 @@
+package org.BenchProject.BenchProject;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(Endpoint.class)
+public class JspControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testJspController() throws Exception {
+		this.mockMvc.perform(get("/name").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+				.andExpect(status().isOk()).andExpect(content().contentType("application/json;charset=UTF-8"));
+
+	}
+
+}
