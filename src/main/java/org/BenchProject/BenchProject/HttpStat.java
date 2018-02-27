33ea4587ef1e2f11ea16480016c62143da58d7e0
HPeterf <horvathpeterf@hotmail.com>
2018-02-27 12:49:33 +0100

    Http státuszkódok

---
+++ src/main/java/org/BenchProject/BenchProject/HttpStat.java
@@ -0,0 +1,25 @@
+package org.BenchProject.BenchProject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HttpStat {
+
+	@RequestMapping(value = "/404", method = RequestMethod.GET)
+	public HttpStatus NotFound() {
+		return HttpStatus.NOT_FOUND;
+	}
+
+	@RequestMapping(value = "/200", method = RequestMethod.GET)
+	public HttpStatus Ok() {
+		return HttpStatus.ACCEPTED;
+	}
+
+	@RequestMapping(value = "/500", method = RequestMethod.GET)
+	public HttpStatus InternalError() {
+		return HttpStatus.INTERNAL_SERVER_ERROR;
+	}
+}
